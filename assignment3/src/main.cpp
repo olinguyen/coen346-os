4b9d27ef27190921edd67d4a839ddd4a03b9f2d5
Space Concordia <olivier@spaceconcordia.ca>
2014-04-13 16:47:45 -0400

    Final submission version

--- assignment3/src/main.cpp
+++ assignment3/src/main.cpp
@@ -3,14 +3,7 @@
 
 using namespace std;
 
-extern int process_to_run;
-extern int thread_count;
-extern int g_time;
 extern double g_clock;
-extern std::deque<process_t> running_queue;
-extern std::deque<process_t> waiting_queue;
-extern pthread_mutex_t thread_flag_mutex;
-extern pthread_cond_t thread_flag_cv;
 
 const char* processes = "processes.txt";
 const char* vm = "vm.txt";
@@ -29,16 +22,7 @@ int main(int argc, const char *argv[])
   vm_manager = get_main_memory(memconfig);
   cmds = read_commands(commands);
   read_processes(processes);
-  init_flag();
-  pthread_t threads[thread_count];
 
-  for (int i = 0; i < thread_count; i++) {
-    pthread_create(&threads[i], NULL, run_process, (void*)&waiting_queue[i]);
-  }
-  start_rr();
-  out_fp = fopen(output_file, "a");
-
-  fclose(out_fp);
 
   return 0;
 }
